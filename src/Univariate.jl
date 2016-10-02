92fb79b2abf6cc22c954804185ddade3ef285e8f
wakakusa <a.beginnerof.kdd.english@gmail.com>
2016-10-02 22:02:11 +0900

    初期登録
    初期登録

--- src/Univariate.jl
+++ src/Univariate.jl
@@ -1,5 +1,71 @@
 module Univariate
 
-# package code goes here
+#パッケージの読み込み
+using  DataFrames , StatsBase #, Gadfly
+
+export NumericSummary,SummaryMerge,Univariate
+
+function NumericSummary(INPUT::DataArray,VarNames::Symbol)  #数値型の基本統計量の算出
+  SummaryVar=var(INPUT)
+  SummaryStd=std(INPUT)
+  SummaryQuartile=quantile( INPUT , [0.00, 0.25, 0.50, 0.75, 1.00])
+  SummaryMedian=median(INPUT)
+  SummaryMean=mean(INPUT)
+
+  Output=DataFrame(colname=VarNames,Var=SummaryVar,Std=SummaryStd,Mean=SummaryMean, Min=SummaryQuartile[1] ,Quartile1st=SummaryQuartile[2] ,Median=SummaryMedian,Quartile3rd=SummaryQuartile[4],Max=SummaryQuartile[5])
+
+  return Output
+end
+
+function NonNumericSummary(INPUT::DataFrame,VarNames::Symbol)  #非数値型の基本統計量の算出
+  Output=by(INPUT, VarNames, df -> size(df, 1))
+
+  return Output
+end
+
+function SummaryMerge(INPUT::DataFrame, Summary::DataFrame ,FirstMergeFlag::Bool)  #データの集約
+  if(FirstMergeFlag ==true)
+  {
+    Summary=INPUT
+    FirstMergeFlag=false
+  } else {
+    Summary=vcat(Summary,INPUT)
+  } end
+
+  return Dict( {"Summary","FirstMergeFlag"},{Summary,FirstMergeFlag})
+end
+
+function Univariate(INPUT)
+  VarNames=names(INPUT)
+  Vartype=eltypes(INPUT)
+  SummaryNonNum=DataFrame(colname="",hist=0)
+  SummaryNum=DataFrame(colname="",Var=0.0,Std=0.0,Mean=0.0, Min=0.0 ,Quartile1st=0.0 ,Median=0.0,Quartile3rd=0.0,Max=0.0)
+  FirstMergeFlagNum=true
+  FirstMergeFlagNonNum=true
+ 
+  for  i = 1 : length(INPUT)
+  {
+    if( ( Vartype[i]==(Int) || Vartype[i]==(Float32)  || Vartype[i]== (Float64) ) == true )
+    {
+      #数値型の基本統計量の算出
+      work=NumericSummary(INPUT[:,i],VarNames[i])
+
+      #基本統計量の集約
+      SummaryNum=SummaryMerge(work,SummaryNum,FirstMergeFlagNum)["Summary"]
+      FirstMergeFlagNum=SummaryMerge(work,SummaryNum,FirstMergeFlagNum)["FirstMergeFlag"]
+    } else  {
+      #非数値型の基本統計量の算出
+      work=NonNumericSummary(INPUT,VarNames[i])
+      
+      #基本統計量の集約
+      SummaryNonNum=SummaryMerge(work,SummaryNonNum,FirstMergeFlagNonNum)["Summary"]
+      FirstMergeFlagNonNum=SummaryMerge(work,SummaryNonNum,FirstMergeFlagNonNum)["FirstMergeFlag"]
+    } end   
+  } end
+
+  return Dict({"SummaryNum","SummaryNonNum"},{SummaryNum ,SummaryNonNum})
+end
+
+
 
 end # module
